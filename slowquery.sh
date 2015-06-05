650076077c1a632bf6acdbd1c88ecf7295e439a8
bunk3r <andrea@purificato.org>
2015-06-05 19:48:09 +0200

    first release

--- slowquery.sh
+++ slowquery.sh
@@ -7,7 +7,7 @@ MY_DIR=$(dirname $(readlink -f $0))
 . $MY_DIR/honeystats.cfg
 
 if [ ! -d "$app/stats" ]; then
-  echo "[!] stats directory doesn't exists ($app/stats):\t let's create it!"
+  echo "[!] stats directory doesn't exists ($app/stats): creating"
   /bin/mkdir $app/stats
   /bin/chmod --recursive 777 $app/stats
 fi
@@ -23,7 +23,7 @@ cd $app/stats
 /bin/rm -f $app/stats/live_dict.txt.tar.gz
 echo "[i] creating 'live_dict.txt'"
 /usr/bin/mysql --user=$mysqluser --password=$mysqlpass --database=$kippodb --execute="select distinct password from auth order by password ASC into outfile '`echo $app`/stats/live_dict.txt';"
-/bin/tar czf $app/stats/live_dict.txt.tar.gz $app/stats/live_dict.txt
+/bin/tar -czf live_dict.txt.tar.gz live_dict.txt
 /bin/rm -f $app/stats/live_dict.txt
 
 #
@@ -33,6 +33,13 @@ echo "[i] creating 'kippo_top15userpass.txt.tmp'"
 /bin/rm -f $app/stats/kippo_top15userpass.txt.tmp
 /usr/bin/mysql --user=$mysqluser --password=$mysqlpass --database=$kippodb --execute="SELECT username,password,count(*) AS count FROM auth GROUP BY USERNAME,PASSWORD ORDER BY count DESC LIMIT 15 into outfile '`echo $app`/stats/kippo_top15userpass.txt.tmp'";
 /bin/mv $app/stats/kippo_top15userpass.txt.tmp $app/stats/kippo_top15userpass.txt
+
+#
+# COPY TXT AND TAR TO FTP
+#
+#cp -r $app/stats/*.txt $mnt/stats
+#cp -r $app/stats/*.tar $mnt/stats
+#chmod --recursive 777 $mnt/stats
 
 echo "[i] applying 777 to stats dir ($app/stats)"
 /bin/chmod --recursive 777 $app/stats
